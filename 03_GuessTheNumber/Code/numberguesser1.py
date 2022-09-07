c242ff01a297276351fadfdc2d1e907050a2f890
SleepDaemon <mahmudesad32@gmail.com>
2022-09-07 20:11:06 +0300

    Fix spacing in guess number

--- 03_GuessTheNumber/Code/numberguesser1.py
+++ 03_GuessTheNumber/Code/numberguesser1.py
@@ -1,13 +1,13 @@
 import random
 num=random.randint(1,20)
 
-user=input ("Hello, What's your name?")
+user=input ("Hello, What's your name?: ")
 print (user)
 print ("Well,", user, "I am thinking of a number between 1 and 20.")
 print ("Guess in 6 tries or less")
 
 for i in range(6):
-    guess=input("take a guess")
+    guess=input("take a guess: ")
     guess=int(guess)
     if guess > num:
         print("too High")
